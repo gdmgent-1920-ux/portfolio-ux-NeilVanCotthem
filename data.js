f7eacf4e54399c6fac595330bf92862792a5183c
NeilVanCotthem <neilvanc@student.arteveldehs.be>
2020-02-29 19:23:36 +0100

    invoegen documenten
    invoegen documenten

--- data.js
+++ data.js
@@ -36,7 +36,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/9fbcf8d8-d034-4629-780d-41e847673907-e98d/",
                 "target": "_blank"
             }
         ]
